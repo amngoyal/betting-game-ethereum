b1521fc7eddb83d2afcb9160c0ac5f122b0e95cf
Aman goyal <im.developer26@gmail.com>
2021-01-25 13:49:38 +0530

    betting game complete

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import { gameAbi, gameAddress } from "./config/bettingGame";
 import "./App.css";
 
 function App() {
+  // ************************************* states *****************************************
   const [networkType, setNetworkType] = useState("");
   const [account, setAccount] = useState("");
   const [token, setToken] = useState(null);
   const [game, setGame] = useState(null);
-  const [web3, setWeb3] = useState(null);
 
   const [isApproved, setIsApproved] = useState(false);
+  const [isBetPlaced, setIsBetPlaced] = useState(false);
+  const [isWinner, setIsWinner] = useState(false);
+  const [reward, setReward] = useState(0);
+  const [generatedNumber, setGeneratedNumber] = useState("");
+  const [currentBalance, setCurrentBalance] = useState(0);
+  const [loading, setLoading] = useState(false);
 
   const [tokenCount, setTokenCount] = useState(50);
   const [selectedNumber, setSelectedNumber] = useState("");
@@ -21,24 +27,36 @@ function App() {
     window.ethereum.enable();
     window.ethereum.autoRefreshOnNetworkChange = false;
     loadBlockchainData();
+
     return () => {};
   }, []);
 
+  // ************************************* load Data *****************************************
   const loadBlockchainData = async () => {
+    console.log("load");
+    // create web3 instance
     const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
-    setWeb3(web3);
     const network = await web3.eth.net.getNetworkType();
     const accounts = await web3.eth.getAccounts();
-    setAccount(accounts[0]);
-    setNetworkType(network);
 
     const token = new web3.eth.Contract(tokenAbi, tokenAddress);
     const gameContract = new web3.eth.Contract(gameAbi, gameAddress);
 
+    setAccount(accounts[0]);
+    setNetworkType(network);
     setToken(token);
     setGame(gameContract);
+
+    await updateBalance(token, accounts[0]);
   };
 
+  // ************************************* Update balance *****************************************
+  const updateBalance = async (token, account) => {
+    const balance = await token.methods.balanceOf(account).call();
+    setCurrentBalance(balance);
+  };
+
+  // ************************************* Place Bet *****************************************
   const placeBet = async (e) => {
     e.preventDefault();
 
@@ -65,24 +83,43 @@ function App() {
     console.log(selectedNumber, seed);
 
     try {
-      const res = await game.methods
+      setLoading(true);
+
+      await game.methods
         .bet(selectedNumber, seed)
         .send({ from: account })
-        .once("receipt", (receipt) => {
-          console.log("REceiPt", receipt);
+        .on("confirmation", function (confNumber, { events }) {
+          setIsBetPlaced(true);
+          const { Bet } = events;
+
+          setReward(Bet.returnValues.reward);
+          setIsWinner(Bet.returnValues.isWinner);
+          setGeneratedNumber(Bet.returnValues.randomNumber);
+
+          console.log(Bet);
+          console.log("called");
+        })
+        .on("error", function (error) {
+          console.log(error);
         });
-      // console.log("response", res);
+      console.log("ouutside ON");
     } catch (err) {
       console.log(err);
+    } finally {
+      setLoading(false);
+      updateBalance(token, account);
     }
 
     console.log("bet placed.");
   };
 
+  // ************************************* approve tokens *****************************************
   const approveTokens = async (e) => {
     e.preventDefault();
     try {
-      const res = await token.methods
+      setLoading(true);
+
+      await token.methods
         .approve(gameAddress, 50)
         .send({ from: account })
         .once("receipt", (receipt) => {
@@ -92,18 +129,39 @@ function App() {
       setIsApproved(true);
     } catch (err) {
       console.log(err);
+      alert("error");
+    } finally {
+      setLoading(false);
     }
   };
 
+  // ************************************* reset *****************************************
+  const reset = (e) => {
+    e.preventDefault();
+
+    setIsApproved(false);
+    setIsBetPlaced(false);
+    setIsWinner(false);
+    setReward(0);
+    setLoading(false);
+    setTokenCount(50);
+    setSelectedNumber("");
+    setSeed("");
+  };
+
+  // ************************************* JSX *****************************************
   return (
     <div className="App">
       <b>Betting Game dApp</b>
       <p>Network: {networkType}</p>
       <p>Account: {account}</p>
+      <p>Current Balance: {currentBalance} APK</p>
+
+      <button onClick={loadBlockchainData}>refresh</button>
 
       <form className="App__form" onSubmit={placeBet}>
         <div>
-          <span>Enter Tokens:</span>
+          <span>Tokens to Bet:</span>
           <input
             value={tokenCount}
             type="number"
@@ -111,9 +169,10 @@ function App() {
             placeholder="Number of tokens"
             onChange={(e) => setTokenCount(e.target.value)}
           />
-          <button disabled={isApproved} onClick={approveTokens}>
-            Approve
+          <button disabled={isApproved || loading} onClick={approveTokens}>
+            {!isApproved && loading ? "wait..." : "Approve"}
           </button>
+          {isApproved && <p>Approved 50 Tokens!!</p>}
         </div>
         <div>
           <span>Select a number (below 99):</span>
@@ -133,10 +192,27 @@ function App() {
             onChange={(e) => setSeed(e.target.value)}
           />
         </div>
-        <button type="submit" disabled={!isApproved}>
-          Submit
-        </button>
+
+        <div>
+          <button
+            type="submit"
+            disabled={!isApproved || loading || isBetPlaced}
+          >
+            {isApproved && loading ? "wait..." : "Submit"}
+          </button>
+          <button disabled={!isBetPlaced} onClick={reset}>
+            reset
+          </button>
+        </div>
       </form>
+      {isBetPlaced && (
+        <>
+          <p>Generated Number: {generatedNumber}</p>
+          <p>
+            you {isWinner ? "Win" : "lose"}, Reward: {reward} APK{" "}
+          </p>
+        </>
+      )}
     </div>
   );
 }
